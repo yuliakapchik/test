588053fad837621ab434cbdc642986e22dbab08b
yuliakapchik <lulu-sun@mail.ru>
2020-10-29 00:04:12 +0300

    create: added advantage and introduction blocks in html and css

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var mySwiper = new Swiper('.articles-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
