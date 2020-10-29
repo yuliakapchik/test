2016f35de9dcdd71c4f60c42c31e1c8be794086b
yuliakapchik <lulu-sun@mail.ru>
2020-10-29 04:33:04 +0300

    create: added block stickers

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var mySwiper = new Swiper('.articles-container', {
+// ----------------Подключение слайдера со статьями----------------------
+var articlesSlider = new Swiper('.articles-container', {
   // Optional parameters
   loop: true,
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
 
   // If we need pagination
   pagination: {
@@ -17,4 +22,48 @@ var mySwiper = new Swiper('.articles-container', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+})
+
+// ------------------------------Подключение слайдера с фото -----------------------
+var gallerySlider = new Swiper('.gallery-container', {
+  // Optional parameters
+  loop: true,
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.gallery-button-next',
+    prevEl: '.gallery-button-prev',
+  },
+  speed: 600,
+})
+
+// ------------------------------Слайдер с отзывами--------------------------------
+var reviewsSlider = new Swiper('.reviews-container', {
+  // Optional parameters
+  loop: true,
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.reviews-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-button-next',
+    prevEl: '.reviews-button-prev',
+  },
+
 })
