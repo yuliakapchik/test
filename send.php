44bbd15aa5e739fa8b5881260d9dd69dd9748e73
yuliakapchik <lulu-sun@mail.ru>
2020-10-30 16:43:34 +0300

    create: added contact form

---
+++ send.php
@@ -0,0 +1,55 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+// Формирование самого письма
+$title = "Новое письмо Тестовое задание";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'ssl://smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = '3367959y@gmail.com'; // Логин на почте
+    $mail->Password   = 'dimochka17122006!'; // Пароль на почте
+    $mail->SMTPSecure = 'SSL';
+    $mail->Port       = 465;
+    $mail->setFrom('3367959y@gmail.com', 'Юлия Капчик'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('lulu-sun@mail.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+
+    } catch (Exception $e) {
+        $result = "error";
+        $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    }
+
+    // Отображение результата
+    // echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+    header('Location: thankyou.html');
